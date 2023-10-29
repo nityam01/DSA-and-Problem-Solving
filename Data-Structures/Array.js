93f7ad41bbd07412e48aabf0da879163244b5aea
nityam01 <nityam0213@gmail.com>
2023-10-29 10:55:45 +0530

    remove-all

--- Data-Structures/Array.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview Array data structure
- *
- * Low level implementation of Array data structure
- * Array are of fixed size and same type, eg. c++,
- *
- * High level implementation of Array data structure
- * Array are of dynamic size and can hold different types of data, eg. JS
- */
